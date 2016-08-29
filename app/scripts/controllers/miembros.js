5550037ee8e7dfefa5f8c507c696307a5920a120
Juan Muñoz <alexx44@users.noreply.github.com>
2016-08-29 17:21:56 -0300

    NGgrid y nueva vista miembros

---
+++ app/scripts/controllers/miembros.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name prototipoApp.controller:MiembrosCtrl
+ * @description
+ * # MiembrosCtrl
+ * Controller of the prototipoApp
+ */
+angular.module('prototipoApp')
+  .controller('MiembrosCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
