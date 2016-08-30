3943bec82c56544e38f92f94e8acc02b4b182e85
Juan Muñoz <alexx44@users.noreply.github.com>
2016-08-30 15:19:08 -0300

    Miembros js

--- app/scripts/controllers/miembros.js
+++ app/scripts/controllers/miembros.js
@@ -25,15 +25,38 @@ angular.module('prototipoApp')
         {field:'fechaUnion', displayName:'Fecha de Unión'},
         {field:'tipoMiembro', displayName:'Tipo de Miembro'}]
 };
-    $scope.showModal=function(){
-        $scope.nuevoMiembro={};
-        var modalInstance = $modal.open({
-            templateUrl: 'views/add-miembros.html',
-            controller: 'AddNuevoMiembroCtrl'
-        })};
-  })
-.controller('AddNuevoMiembroCtrl', function($scope, $modalInstance){
-   $scope.cancel=function(){
+
+$scope.showModal=function () {
+$scope.nuevoMiembro={};
+    var modalInstance = $modal.open({
+      templateUrl: 'views/add-miembros.html',
+      controller:'AddNuevoMiembroCtrl',
+                resolve: {
+                    nuevoMiembro: function() {
+                        return $scope.nuevoMiembro;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function(selectedItem) {
+                   $scope.miembros.push({
+                    no: $scope.miembros.length + 1,
+                    nombre: $scope.nuevoMiembro.nombre,
+                    tipoMiembro: $scope.nuevoMiembro.tipoMiembro,
+                    fidelidad: $scope.nuevoMiembro.fidelidad,
+                    fechaUnion: $scope.nuevoMiembro.fechaUnion
+
+                });
+            });
+        };
+
+    })
+  .controller('AddNuevoMiembroCtrl', function ($scope, $modalInstance,nuevoMiembro) {
+    $scope.nuevoMiembro=nuevoMiembro;
+    $scope.salvarNuevoMiembro=function(){
+        $modalInstance.close(nuevoMiembro);
+    };
+    $scope.cancel=function(){
        $modalInstance.dismiss('cancel');
    };
-})
+});
