8c5751907fbf1c141216d954ed137eabbc372ada
Juan Muñoz <cl.jmunoz@gmail.com>
2016-08-30 00:10:17 -0300

    ng-grid

--- app/scripts/controllers/miembros.js
+++ app/scripts/controllers/miembros.js
@@ -8,10 +8,21 @@
  * Controller of the prototipoApp
  */
 angular.module('prototipoApp')
-  .controller('MiembrosCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MiembrosCtrl', function ($scope,$http) {
+    $http.get('http://localhost:9000/miembros.json').success(function(data){
+    $scope.miembros = data;
+});
+    $scope.gridOptions = { 
+    data: 'miembros',
+    showGroupPanel: true,
+    enableCellSelection: true,
+    enableRowSelection: false,
+    enableCellEdit: true,
+    columnDefs: [
+        {field:'no', displayName:'Nº.'}, 
+        {field:'nombre', displayName:'Nombre'}, 
+        {field:'fidelidad', displayName:'Puntos Fidelidad'},
+        {field:'fechaUnion', displayName:'Fecha de Unión'},
+        {field:'tipoMiembro', displayName:'Tipo de Miembro'}]
+};
   });
